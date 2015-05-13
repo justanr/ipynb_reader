84cc53c5766cb9c0decded0e08eaac0e2fbc5725
justanr <alecreiter@gmail.com>
2015-05-13 08:52:56 -0400

    setup.py to install locally

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name="ipynb_reader",
+        version="0.0.1",
+        author="Alec Nikolas Reiter",
+        author_email="alecreiter@gmail.com",
+        description="Transform IPython Notebooks to Pelican posts."
+        )
